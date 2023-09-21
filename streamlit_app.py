32e96e0a15c164912e56c3f59c6bfa2d62502921
am0330 <144980841+am0330@users.noreply.github.com>
2023-09-21 04:44:31 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ import random
 st.title('おみくじアプリ')
 
 if st.button("おみくじを引く"):
-   results=["メガビッグ","ロト7","ハロウィンジャンボ","大吉","中吉","小吉","吉","凶","大凶"]
+   results=["メガビッグ","ロト7","ハロウィンジャンボ","10億","大吉","中吉","小吉","吉","凶","大凶"]
    result=random.choice(results)
    st.write(f"結果:{result}")
