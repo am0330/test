a46f8a1b05538061201854a64389706f9405eaff
am0330 <144980841+am0330@users.noreply.github.com>
2023-09-21 04:31:20 +0000

    aiyeo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,12 @@
 import streamlit as st
 import random
 
-# ページ設定（タブに表示されるタイトル、表示幅）
-st.set_page_config(page_title="タイトル", layout="wide")
+
 
 # タイトルを設定
 st.title('おみくじアプリ')
 
-if st.button("おみくじを引く")
+if st.button("おみくじを引く"):
    results=["大吉","中吉","小吉","吉","凶","大凶"]
    result=random.choice(results)
    st.write("結果:{result}")
